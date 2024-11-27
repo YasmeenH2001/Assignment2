7453b012d1d8ba7bd769d59c23c378c754fd11fc
YasmeenH2001 <jamria2001@gmail.com>
2024-11-27 20:47:11 +0300

    updated

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php 
 $url= "https://data.gov.bh/api/explore/v2.1/catalog/datasets/01-statistics-of-students-nationalities_updated/records?where=colleges%20like%20%22IT%22%20AND%20the_programs%20like%20%22bachelor%22&limit=100"; 
-$response= file_get_contents($url);
+$jsonFilePath = 'student_enrollment_data.json';
+$jsonData= file_get_contents($jsonFilePath);
 $data = json_decode($response , true); 
 if(!$data || !isset($data["results"])){ die('Error fetching the data from API'); } 
 $result=$data["results"]; 
