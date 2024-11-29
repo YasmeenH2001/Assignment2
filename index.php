f9ad2b1acf62300a649f6c69b9fb2af37d1e5cb4
YasmeenH2001 <jamria2001@gmail.com>
2024-11-30 00:04:15 +0300

    updated

--- index.php
+++ index.php
@@ -1,18 +1,17 @@
-<?php 
-$url= "https://data.gov.bh/api/explore/v2.1/catalog/datasets/01-statistics-of-students-nationalities_updated/records?where=colleges%20like%20%22IT%22%20AND%20the_programs%20like%20%22bachelor%22&limit=100"; 
-$jsonFilePath = 'student_enrollment_data.json';
-$jsonData= file_get_contents($jsonFilePath);
-$data = json_decode($response , true); 
-if(!$data || !isset($data["results"])){ die('Error fetching the data from API'); } 
-$result=$data["results"]; 
-?> 
+<?php  
+ $url = "https://data.gov.bh/api/explore/v2.1/catalog/datasets/01-statistics-of-students-nationalities_updated/records?where=colleges%20like%20%22IT%22%20AND%20the_programs%20like%20%22bachelor%22&limit=100";  
+  $response = file_get_contents($url); 
+  $data = json_decode($response, true);  
+     if(!$data || !isset($data["results"])){    
+         die('error fetching the data from API'); } 
+         $result = $data["results"]; 
+         ?>
+          <html> 
+        <head> 
+        <meta name="viewport" content="width=device-width, initial-scale=1">   
+              <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css"> 
+                        
 
-<!DOCTYPE html>
-<html>
-<head>
-    <title>UOB Student Nationalities</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css">
     <style>
         body {
             font-family: Arial, sans-serif;
